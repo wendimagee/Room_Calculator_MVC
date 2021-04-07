465bdca84c5929de99bf17c8e8cf9e43f95e6b1d
Wendi Magee <wendimagee@gmail.com>
2021-04-07 10:12:49 -0400

    Add project files.

---
+++ Room_Calculator/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Room_Calculator.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Room_Calculator.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        //this handles the view
+        public IActionResult Index()
+        {
+            return View();
+        }
+        //this overloads to handle post info
+        [HttpPost]
+        public IActionResult Index(Room room)
+        {
+            return RedirectToAction("Result", room);
+        }
+        public IActionResult Result(Room r)
+        {
+            return View(r);
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Room_Calculator/Models/Room.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Room_Calculator.Models
+{
+    public class Room
+    {
+        public double Length { get; set; }
+        public double Height { get; set; }
+        public double Width { get; set; }
+        public double area { get { return Length * Width; } }
+        public double perimeter { get {return (Length * 2) + (2 * Width); } }
+        public double volume { get { return Length * Height * Width; } }
+    }
+}
